a57f2e8cfe1912c9b68fd1d20f1967e70449a7ff
AbasBashir <MrAbasBashir@gmail.com>
2023-12-23 11:00:06 +0100

    Created dynamic elements + added image icons for functionality usage later

--- app.js
+++ app.js
@@ -1,2 +1,45 @@
 
 
+// Globale variables
+
+let addTask = document.getElementById('add-btn');
+
+let inputTask = document.getElementById('input-task');
+
+let taskContainer = document.getElementById('task-container');
+
+
+// Event Listener for Add Button
+addTask.addEventListener('click', function(){
+
+    let task = document.createElement('div');
+
+    task.classList.add('task'); 
+
+    // Creating our list item and appending to taskContainer
+    let li = document.createElement('li');
+    li.innerText = `${inputTask.value}`;    
+    task.append(li);
+
+    let checkBtn = document.createElement('img');
+
+    checkBtn.setAttribute('src', 'images/unReadStatus.svg');
+
+    task.append(checkBtn);
+
+    let trashBtn = document.createElement('img');
+
+    trashBtn.setAttribute('src', 'images/trash.svg');
+
+    task.append(trashBtn);
+
+    if (inputTask.value === "") {
+        
+        alert('Please Enter A Task');
+
+    }else{
+        taskContainer.appendChild(task);
+    }
+
+    inputTask.value = "";
+})
